b4d54ed09998f2c8e97361b00600738e32b72235
First-project <mehedihasan0937580@gmail.com>
2024-11-21 00:24:50 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="text-center text-3xl bg-sky-300 text-primary py-1 font-Mplus1 ">
+          Welcome to my finance website
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/* eslint-disable no-undef */
+
+/** @type {import('tailwindcss').Config} */
+const { nextui } = require("@nextui-org/react");
+
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    container: { center: true },
+    extend: {
+      colors: {
+        primary: "#772ab3",
+        secondary: "#adb2b1",
+        foreground: "#ffffff",
+      },
+      fontFamily: {
+        Mplus1: '"M PLUS 1", sans-serif',
+      },
+    },
+  },
+  darkMode: "class",
+  plugins: [nextui()],
+};
