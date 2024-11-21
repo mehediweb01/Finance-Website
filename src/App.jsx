66b5c0cd6c0791f27b264a6262293c1f64015994
First-project <mehedihasan0937580@gmail.com>
2024-11-22 00:27:57 +0600

    vision section done

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Hero from "./components/Hero";
 import NavbarArea from "./components/Navbar";
+import Vision from "./components/Vision";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
         <NavbarArea />
         <div className="overflow-x-hidden px-8 sm:px-12 md:px-16 lg:px-20" >
           <Hero />
+          <Vision />
         </div>
       </div>
     </>
---
+++ src/components/Vision.jsx
@@ -0,0 +1,17 @@
+const Vision = () => {
+  return (
+    <section className="pb-8 sm:mt-56 mt-20">
+      <div className="bg-[#3D3D543D] py-8 px-16 rounded-md ">
+        <h3 className="font-bold text-3xl text-white text-center leading-10">
+          Wern&apos;s Vision
+        </h3>
+        <p className="text-center text-primary font-semibold text-3xl sm:text-[40px] sm:leading-[56px] leading-10 ">
+          Building a financial future where everyone has access to secure and
+          effortless digital money.
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default Vision;
