11305c20f7503d6853b302efb6c38d05ed3164af
First-project <mehedihasan0937580@gmail.com>
2024-11-22 00:59:52 +0600

    Offer section complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Hero from "./components/Hero";
 import NavbarArea from "./components/Navbar";
+import Offer from "./components/Offer";
 import Vision from "./components/Vision";
 
 function App() {
@@ -7,9 +8,10 @@ function App() {
     <>
       <div>
         <NavbarArea />
-        <div className="overflow-x-hidden px-8 sm:px-12 md:px-16 lg:px-20" >
+        <div className="overflow-hidden px-8 sm:px-12 md:px-16 lg:px-20" >
           <Hero />
           <Vision />
+          <Offer />
         </div>
       </div>
     </>
---
+++ src/components/Offer.jsx
@@ -0,0 +1,53 @@
+import dollar from "/dollar.png";
+import ramp from "/ramp.png";
+import solution from "/solution.png";
+
+const Offer = () => {
+  const offerData = [
+    {
+      heading: "Cardano Debit-Cards",
+      descrption:
+        "Cardano, simplified. The debit card that makes crypto spending easy.",
+      img_url: dollar,
+    },
+    {
+      heading: "On & Off Ramp",
+      descrption: "Fiat meets crypto, effortlessly buy, sell, and manage.",
+      img_url: ramp,
+    },
+    {
+      heading: "Solutions for business",
+      descrption:
+        "Empowering businesses with effortless crypto payments and seamless fiat solutions.",
+      img_url: solution,
+    },
+  ];
+  return (
+    <section className="sm:mt-24 mt-16 py-6">
+      <div>
+        <h1 className="font-bold md:text-[40px] text-3xl md:leading-10 leading-6 text-center text-white">
+          What do we offer?
+        </h1>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 items-center justify-center gap-6 mt-10">
+          {offerData.map((item, index) => (
+            <div key={index} className="flex justify-center items-start gap-3">
+              <div className="bg-[#3D3F54] rounded-[24px] max-w-[66px] max-h-[66px] p-4 ">
+                <img src={item.img_url} alt="" className="size-[34px]" />
+              </div>
+              <div>
+                <h2 className="text-white font-bold leading-9 text-2xl">
+                  {item.heading}
+                </h2>
+                <p className="text-secondary text-base leading-6">
+                  {item.descrption}
+                </p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Offer;
